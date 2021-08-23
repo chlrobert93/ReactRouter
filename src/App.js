6e2f534c1167b1e983f44dcdae5aef5d42d3b869
Eder <chlrobert93@gmail.com>
2021-08-23 16:43:11 -0500

    Creación de componentes Wrapper

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import {Route,Switch} from 'react-router-dom';
 import AllMeetup from './pages/AllMeetup';
 import Favorites from './pages/Favorites';
 import NewMeetup from './pages/NewMeetup';
-import MainNavigation from './Components/Layout/MainNavigation';
+import Layout from './Components/Layout/Layout';
+
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route exact path="/">
-          <AllMeetup />
-        </Route>
-        <Route exact path="/favorites">
-          <Favorites />
-        </Route>
-        <Route exact path="/new/meetup" component={NewMeetup} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <AllMeetup />
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites />
+          </Route>
+          <Route exact path="/new/meetup" component={NewMeetup} />
+        </Switch>
+      </Layout>
     </div>
   );
 }
